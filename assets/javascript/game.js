df1ee25f4b4e4a12b6097692682bfc177552db39
myles-dewaal <mylesdewaal@hotmail.com>
2019-09-11 22:44:51 -0600

    finished jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,68 @@
 var won = 0;
 var lost = 0;
-var randomNuber;
-var targetNumber = [45];
+var randomNumber;
 var counter = 0;
 var crystal1;
 var crystal2;
 var crystal3;
 var crystal4;
+var resetGame = false;
 
-
-$("#number").text(targetNumber);
+ranNumber();
+$("#number").text(randomNumber);
 var imageCrystal = $(".crystal");
 //for loop
-    for (var i = 0; i < numberOptions.length; i++) {
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+// for (var i = 0; i < numberOptions.length; i++) {
+//     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
-    }
+// }
+
+function reset() {
+    ranNumber()
+    counter = 0;
+    $("#number").text(randomNumber);
+    $("#score").text(counter);
+    resetGame = true;
+
+}
 $(".crystal").on("click", function () {
+    if (resetGame) {
+        $("#winLose").empty();
+        resetGame = false;
+    }    
+    var crystalValue = $(this).attr("id");
+    if (crystalValue === "crystal1") {
+        counter = counter + crystal1
+    } else if (crystalValue === "crystal2") {
+        counter = counter + crystal2
+    } else if (crystalValue === "crystal3") {
+        counter = counter + crystal3
+    } else if (crystalValue === "crystal4") {
+        counter = counter + crystal4
+    }
 
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
 
     $("#score").text(counter);
 
-    if (counter == targetNumber) {
-        alert("You win!");
+    if (counter === randomNumber) {
+        reset();
+        won ++;
+        $("#wins").text(won);
+        $("#winLose").text("You win!")
     }
 
-    else if (counter > targetNumber) {
-        alert("You lose!!");
+    else if (counter > randomNumber) {
+        reset();
+        lost ++;
+        $("#lost").text(lost);
+        $("#winLose").text("You loose!")
     }
 
 });
-    function ranNumber() {
-        crystal1 = Math.floor(Math.random() * 9 ) + 1;
-        crystal2 = Math.floor(Math.random() * 9 ) + 1;
-        crystal3 = Math.floor(Math.random() * 9 ) + 1;
-        crystal4 = Math.floor(Math.random() * 9 ) + 1;
-        randomNuber = Math.floor(Math.random() * ((120-19)+) ) 
-    }
-    
+function ranNumber() {
+    crystal1 = Math.floor(Math.random() * 9) + 1;
+    crystal2 = Math.floor(Math.random() * 9) + 1;
+    crystal3 = Math.floor(Math.random() * 9) + 1;
+    crystal4 = Math.floor(Math.random() * 9) + 1;
+    randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19)
+}
